e1f06bf6d9a447c332504004fb23f8d2a8eef55b
Sa-lile <77283872+Sa-lile@users.noreply.github.com>
2024-03-21 14:50:58 +0100

    Add files via upload

---
+++ ImanyuDemo/MatchCase/sample.py
@@ -0,0 +1,20 @@
+color = 'red'
+
+match color:
+    case 'red':
+        print('This is red')
+    case 'green':
+        print('This is green')
+    case 'blue':
+        print('This is blue')
+    case _:
+         print('Other color')
+
+# if color == 'red':
+#     print('This is red')
+# elif color == 'green':
+#     print('This is green')
+# elif color == 'blue':
+#     print('This is blue')
+# else:
+#     print('Other color')
